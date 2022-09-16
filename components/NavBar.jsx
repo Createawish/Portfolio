533097b56595e55ab25c9264132fd4839e5405e4
Createawish <bezlyudovalera1996@gmail.com>
2022-09-17 02:13:28 +0300

    Feature: realization functionalyti of burger-menu

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Image from "next/image";
 import LOGO from '../public/assets/main-logo.png';
 import Link from "next/link";
@@ -8,6 +8,12 @@ import {BsFillPersonLinesFill} from "react-icons/bs";
 
 
 const NavBar = () => {
+    const [nav, setNav] = useState(false);
+
+    const handleClose = () =>{
+        setNav(!nav)
+    }
+
     return (
             <div className='fixed w-full h-20 shadow-xl z-[100]'>
                 <div className='flex justify-between items-center w-full px-2 2xl:px-16'>
@@ -30,17 +36,22 @@ const NavBar = () => {
                             <li className='ml-10 text-sm uppercase hover:border-b'>Contact</li>
                         </Link>
                     </ul>
-                    <div className='md:hidden'>
+                    <div className='md:hidden' onClick={handleClose}>
                         <AiOutlineMenu size={25}/>
                     </div>
                 </div>
                 </div>
-                <div className='fixed left-0 top-0 w-full h-screen bg-black/70'>
-                    <div className='fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in-duration-500'>
+
+
+
+                <div className={nav ? 'fixed left-0 top-0 w-full h-screen bg-black/70' : ''}>
+                    <div className={nav
+                        ?'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in-duration-500'
+                        :'fixed left-[-100%] top-0 p-10 ease-in-duration-500'}>
                         <div>
                             <div className='flex w-full items-center justify-between' >
                                 <Image src={LOGO} alt='logo' width='87' height='35'/>
-                                <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+                                <div onClick={handleClose} className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
                                     <AiOutlineClose/>
                                 </div>
                             </div>
