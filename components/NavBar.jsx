d0e4e742da8f264407fa46eab45c2f406bdc0a18
Createawish <bezlyudovalera1996@gmail.com>
2022-09-17 01:52:55 +0300

    :Feature: add burger-menu

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import Image from "next/image";
 import LOGO from '../public/assets/main-logo.png';
 import Link from "next/link";
+import {AiOutlineClose, AiOutlineMenu, AiOutlineMail} from "react-icons/ai";
+import {FaLinkedin, FaGithub } from 'react-icons/fa'
+import {BsFillPersonLinesFill} from "react-icons/bs";
 
 
 const NavBar = () => {
@@ -27,8 +30,64 @@ const NavBar = () => {
                             <li className='ml-10 text-sm uppercase hover:border-b'>Contact</li>
                         </Link>
                     </ul>
+                    <div className='md:hidden'>
+                        <AiOutlineMenu size={25}/>
+                    </div>
                 </div>
                 </div>
+                <div className='fixed left-0 top-0 w-full h-screen bg-black/70'>
+                    <div className='fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in-duration-500'>
+                        <div>
+                            <div className='flex w-full items-center justify-between' >
+                                <Image src={LOGO} alt='logo' width='87' height='35'/>
+                                <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+                                    <AiOutlineClose/>
+                                </div>
+                            </div>
+                            <div className='border-b border-gray-300 my-4'>
+                                <p className='w-[85%] mdw-[90%]'>Let's build something legendary together</p>
+                            </div>
+                        </div>
+                            <div className='py-4 flex flex-col'>
+                                <ul className='uppercase'>
+                                    <Link href='/'>
+                                        <li className='py-4 text-sm'>Home</li>
+                                    </Link>
+                                    <Link href='/'>
+                                        <li className='py-4 text-sm'>About</li>
+                                    </Link>
+                                    <Link href='/'>
+                                        <li className='py-4 text-sm'>Skills</li>
+                                    </Link>
+                                    <Link href='/'>
+                                        <li className='py-4 text-sm'>Project</li>
+                                    </Link>
+                                    <Link href='/'>
+                                        <li className='py-4 text-sm'>Contact</li>
+                                    </Link>
+                                </ul>
+                                <div className='pt-40'>
+                                    <p className='uppercase tracking-widest text-[#5651e5]'>Let's Connect</p>
+                                    <div className='flex justify-between items-center my-4 w-full sm:w-[100%]'>
+                                        <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration'>
+                                        <FaLinkedin/>
+                                        </div>
+                                        <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration'>
+                                            <FaGithub/>
+                                        </div>
+                                        <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration'>
+                                            <AiOutlineMail/>
+                                        </div>
+                                        <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration'>
+                                            <BsFillPersonLinesFill/>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+
+
             </div>
 
     );
