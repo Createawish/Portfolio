606a60250d46484fe43c9f2de4a00c5bf65bfd02
Createawish <bezlyudovalera1996@gmail.com>
2022-09-19 14:43:37 +0300

    Fix: form in contact component

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -62,7 +62,7 @@ console.log(query)
                     {/*right*/}
 <div className='col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4'>
     <div className='p-4'>
-        <form onClick={formSubmit} >
+        <form onSubmit={formSubmit} >
             <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
                 <div className='flex flex-col'>
                     <label className='uppercase text-sm py-2'>Name</label>
@@ -86,7 +86,7 @@ console.log(query)
                 <label className='uppercase text-sm py-2'>Massage</label>
                <textarea name='massage' required value={query.message} onChange={handleParam()} className='border-2 rounded-lg p-3 border-gray-300' rows='10'></textarea>
             </div>
-            <button className='w-full p-4 text-gray-100 mt-4'>Send Message</button>
+            <button type='submit' className='w-full p-4 text-gray-100 mt-4'>Send Message</button>
         </form>
     </div>
 </div>
